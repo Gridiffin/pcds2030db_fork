a0e9cc85a1745c601af58e4cd5b5b808735b748e
sjnbu <iamrazer123@gmail.com>
2025-06-24 11:48:16 +0800

    Fix Redirect

--- app/views/agency/outcomes/create_outcome.php
+++ app/views/agency/outcomes/create_outcome.php
@@ -173,7 +173,7 @@ require_once '../../layouts/page_header.php';
                     <input type="hidden" name="is_draft" id="isDraftInput" value="0" />
                     <button type="submit" class="btn btn-success" id="saveBtn" onclick="document.getElementById('isDraftInput').value='0';">Save Outcome</button>
                     <button type="submit" class="btn btn-warning ms-2" id="saveDraftBtn" onclick="document.getElementById('isDraftInput').value='1';">Save as Draft</button>
-                    <a href="manage_outcomes.php" class="btn btn-secondary ms-2">Cancel</a>
+                    <a href="submit_outcomes.php" class="btn btn-secondary ms-2">Cancel</a>
                 </div>
             </form>
         </div>
