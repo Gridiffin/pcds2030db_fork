0403a1055de2f710e895ba4e43a95dbdfa76cf0c
Milizee <gridiffin6603@gmail.com>
2025-07-16 11:45:42 +0800

    delete unused files

--- app/views/agency/outcomes/create_outcomes_detail.php
+++
@@ -1,8 +0,0 @@
-<?php
-// This file is deprecated. All create outcome details functionality has been removed.
-// Please use the Submit Outcomes page for all outcome management.
-?>
-<!-- This page is deprecated and intentionally left blank. -->
-
-
-
